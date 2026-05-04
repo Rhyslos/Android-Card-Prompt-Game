241a81dc017990bb700fa03f3d2da0696822415e
Yuna Emiya <yuna-emiya@outlook.com>
2026-05-05 00:30:50 +0200

    Trying to make a solution to add buttons to the main screen to add player names

--- app/src/main/java/com/example/cardgame/MainActivity.kt
+++ app/src/main/java/com/example/cardgame/MainActivity.kt
@@ -4,11 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import com.example.cardgame.ui.screens.PrintHelloWorld
+import com.example.cardgame.ui.screens.AddPlayerButton
 import com.example.cardgame.ui.theme.CardGameTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,8 +20,9 @@ class MainActivity : ComponentActivity() {
             CardGameTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     // THIS IS THE FUNCTION CALL
-                    PrintHelloWorld(
-                        modifier = Modifier.padding(innerPadding)
+                    AddPlayerButton(
+                        modifier = Modifier.padding(innerPadding),
+                        onClick = {}
                     )
                 }
             }
--- app/src/main/java/com/example/cardgame/ui/screens/PlayerSetupScreen.kt
+++ app/src/main/java/com/example/cardgame/ui/screens/PlayerSetupScreen.kt
@@ -1,13 +1,44 @@
 package com.example.cardgame.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.runtime.Composable
 import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 @Composable
 fun PrintHelloWorld(modifier: Modifier) {
-    Text(
-        text = "Hello, New World from new file!",
-        modifier = Modifier
-    )
+    Column {
+        Text(
+            text = "Hello, New World from new!",
+            modifier = Modifier
+        )
+    }
+}
+
+fun AddNamesToList() {
+
+}
+
+@Composable
+fun AddPlayerButton(modifier: Modifier = Modifier, onClick: () -> Unit){
+    Column(
+        modifier = Modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        ElevatedButton(onClick = onClick, modifier = Modifier,
+            ) {
+            Text( text = "Add Player")
+        }
+        ElevatedButton(onClick = onClick, modifier = Modifier ) {
+            Text( text = "Add Player")
+        }
+        ElevatedButton(onClick = onClick, modifier = Modifier ) {
+            Text( text = "Add Player")
+        }
+    }
 }
